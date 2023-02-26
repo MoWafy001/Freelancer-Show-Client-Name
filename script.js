c268c26464ad3be0abac4bdfaf9b9f98869e5aec
mowafy001 <wafy123445@gmail.com>
2023-02-26 13:12:00 +0200

    finish

--- script.js
+++ script.js
@@ -1,9 +1,60 @@
-var url = document.URL.split("projects/")[1].trim()
-if(url.endsWith("/")) url = url.slice(0, url.length - 1)
+var url;
+var project;
+var user;
+
+const getSEOUrl = () => {
+  try{
+    return document.URL.match("projects/(.+)/details")[0].replace("/details","").replace("projects/","")
+  }catch(e){
+    return null
+  }
+}
 
 const getProject = async seoUrl => {
   const project = await fetch("https://www.freelancer.com/api/projects/0.1/projects?seo_urls%5B%5D=" + seoUrl)
                   .then(res => res.json())
                   .then(data => data.result.projects[0])
+
+  return project
 }
-const getUser = userId => {}
+
+const getUser = async userId => {
+  const user = await fetch("https://www.freelancer.com/api/users/0.1/users/" + userId)
+                  .then(res => res.json())
+                  .then(data => data.result)
+                  
+  return user
+}
+
+const displayClientName = (publicName, username) => {
+  let element
+  let c = 0;
+
+  const i = setInterval(()=>{
+    element = [...document.querySelectorAll("fl-card-header-title")].find(e=>e.innerText==="About the Client") 
+    if(element || c >= 5){
+      clearInterval(i)
+    }else{
+      c++;
+      return
+    }
+    element.innerHTML += `
+      <a href="https://www.freelancer.com/u/${username}" style="color:#0060df">${publicName}</a>
+    `
+  },1000)
+}
+
+const main = async () => {
+  url = getSEOUrl()
+  if(!url){
+    console.log("invalid URL")
+    return
+  }
+
+  project = await getProject(url)
+  user = await getUser(project.owner_id)
+
+  displayClientName(user.public_name, user.username)
+}
+
+main()
